33559f581ebc6073a72ea0e1cae34104d7ca8e5a
service3 <service3@msu.edu>
2023-04-10 19:33:19 -0400

    Basic code cleaning moved two classes into separate python files

---
+++ envcell.py
@@ -0,0 +1,82 @@
+import pygame
+
+
+def grass_color_gradient(x):
+    """
+    Dirt to grass gradient generated from colordesigner.io/gradient-generator.
+    Used website because RGB color space is nonlinear apparently and my homemade
+    interpolation didn't work
+
+    x is the current grass amount divided by the maximum grass (set to 50)
+
+    (205, 133, 63)
+    (193, 134, 50)
+    (180, 136, 38)
+    (165, 137, 27)
+    (150, 138, 16)
+    (133, 139, 8)
+    (114, 140, 7)
+    (94, 140, 13)
+    (69, 140, 23)
+    (34, 139, 34)
+
+    Smooth interpolation may be possible through some other method.
+    Look into HSV or HSL
+    """
+    if 0 <= x and x < 0.1:
+        return (205, 133, 63)
+    elif 0.1 <= x and x < 0.2:
+        return (193, 134, 50)
+    elif 0.2 <= x and x < 0.3:
+        return (180, 136, 38)
+    elif 0.3 <= x and x < 0.4:
+        return (165, 137, 27)
+    elif 0.4 <= x and x < 0.5:
+        return (150, 138, 16)
+    elif 0.5 <= x and x < 0.6:
+        return (133, 139, 8)
+    elif 0.6 <= x and x < 0.7:
+        return (114, 140, 7)
+    elif 0.7 <= x and x < 0.8:
+        return (94, 140, 13)
+    elif 0.8 <= x and x < 0.9:
+        return (69, 140, 23)
+    elif 0.9 <= x and x <= 1.0:
+        return (34, 139, 34)
+    else:
+        return (205, 133, 63)
+
+
+class EnvCell(pygame.sprite.Sprite):
+    """
+    Cells for the environment grass grid. Inherits from pygame sprite class
+    for drawing to the screen
+    """
+
+    def __init__(self, x, y, id_x, id_y, width, height, grass):
+        super().__init__()
+        self.pos_x = x
+        self.pos_y = y
+        self.id_x = id_x
+        self.id_y = id_y
+        self.width = width
+        self.height = height
+        self.grass = grass
+
+        # Pygame stuff for drawing cell to screen
+        self.image = pygame.Surface([self.width, self.height])
+        self.image.fill(grass_color_gradient(self.grass / 50))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [self.pos_x, self.pos_y]
+
+    def update(self, grid):
+        # gets the grass amount from the environment grid
+        self.grass = grid[self.id_y, self.id_x]
+
+        # updates its color in accordance with the grass_color_gradient function
+        self.image.fill(grass_color_gradient(self.grass / 50))
+
+        # NOTE: 50 is the maximum grass amount per cell as decided in the
+        # advance_grid function. It's probably best to find a way to pass
+        # this variable to the update function to avoid future issues,
+        # should we decide we want the maximum grass amount to vary
---
+++ herbivore.py
@@ -0,0 +1,75 @@
+import numpy as np
+import pygame
+
+
+class Creature:
+    """
+    General behavioral functions can go here
+    """
+
+    pass
+
+
+class Herbivore(pygame.sprite.Sprite, Creature):
+    def __init__(self, genes, x, y, orientation, strength=0):
+        super().__init__()
+        # position information
+        self.angle = orientation
+        self.normal = np.array([np.cos(self.angle), np.sin(self.angle)])
+        self.pos = np.array([x, y])
+
+        # genome information
+        self.genes = genes
+        self.strength = strength
+        self.color = genes["color"]
+
+        # defining state variables
+        self.energy = np.mean(
+            self.genes["max-energy"]
+        )  # averaging value from both chromosomes
+        self.hunger = np.mean(self.genes["max-energy"]) - self.energy
+        self.desire_mate = 0
+
+        # pygame drawing information
+        self.picture = pygame.image.load(
+            "base-herbivore.png"
+        ).convert_alpha()  # converting makes draw time faster I guess
+        self.picture = pygame.transform.scale(self.picture, (20, 20))
+        # base-herbivore.png is white so the next line
+        # tints it to the be color determined by it's genes
+        self.picture.fill(self.color, special_flags=pygame.BLEND_MULT)
+
+        self.image = self.rotate(self.picture, -self.angle)
+        self.rect = self.image.get_rect()
+        self.rect.center = [round(self.pos[0]), round(self.pos[1])]
+
+    def rotate(self, surface, angle):
+        """
+        This function exists because pygame's built in rotation functions
+        don't work well. They degrade the image quality and eventually
+        crash the program. See documentation for more information
+        """
+        rotated_surface = pygame.transform.rotozoom(surface, angle * 180 / np.pi, 1)
+        return rotated_surface
+
+    def update(self, grid, dt):
+        # updates the angle by the turn speed
+        self.angle += np.mean(self.genes["turn-speed"]) * dt
+
+        # updates its orientation vector with new angle
+        self.normal = np.array([np.cos(self.angle), np.sin(self.angle)])
+
+        # updates its position with its speed and new normal vector
+        self.pos = self.pos + self.normal * np.mean(self.genes["speed"]) * dt
+
+        # rotates the image according to new angle
+        self.image = self.rotate(self.picture, -self.angle)
+        self.rect = self.image.get_rect()
+
+        # updates the draw position based on new position vector
+        self.rect.center = [round(self.pos[0]), round(self.pos[1])]
+
+        # eat the grass
+        column = int(self.pos[0] / 25)
+        row = int(self.pos[1] / 25)
+        grid[row, column] = 0  # I'm honestly not sure yet why the row and column is switched, still have to look closer
